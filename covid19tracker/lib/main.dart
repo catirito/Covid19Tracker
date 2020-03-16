a65d583e81327a2562dbd4b95f89e4db819071ab
Bruno Del Greco <catirito30@gmail.com>
2020-03-16 14:22:06 +0100

    Add favorites and new view

--- covid19tracker/lib/main.dart
+++ covid19tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',            
+      title: 'Startup Name Generator',  
+      theme: ThemeData(          // Add the 3 lines from here... 
+        primaryColor: Colors.blueGrey,
+      ),          
       home: RandomWords(),
     );
   }
@@ -20,6 +24,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 14.0);
 
   @override
@@ -33,29 +38,85 @@ class RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
-        return _buildRow(_suggestions[index], 1000);
+        return _buildRow(_suggestions[index], _randomNumber());
       });
   }
 
+  int _randomNumber() {
+    var rng = new Random();
+    return rng.nextInt(1000000);
+  }
+
   Widget _buildRow(WordPair pair, int total) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-    trailing: Text(
-      total.toString()
-    ),
-  );
-}
+    final bool alreadySaved = _saved.contains(pair); 
+    return ListTile(
+      title: Text(
+        total.toString(),
+        style: _biggerFont,
+      ),
+      leading: Text(
+        pair.asPascalCase,
+        
+      ),
+      dense: false,
+      trailing: Icon(   // Add the lines from here... 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ), 
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else { 
+            _saved.add(pair); 
+          } 
+        });
+    },   
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
       title: Text('Startup Name Generator'),
+      actions: <Widget>[      // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],  
     ),
     body: _buildSuggestions(),
   );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(   // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+
+            return Scaffold(         // Add 6 lines from here...
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            ); 
+        },
+      ),
+    );
   }
 }
