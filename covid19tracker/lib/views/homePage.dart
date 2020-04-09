0e0f5947b9277184e375d354577dfcae8f4d8342
Bruno Del Greco <catirito30@gmail.com>
2020-04-09 17:39:47 +0200

    Most affected countries

--- covid19tracker/lib/views/homePage.dart
+++ covid19tracker/lib/views/homePage.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
+import 'package:Covid19Tracker/views/panels/mostAffectedCountries.dart';
+
 import './panels/worldPanel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,18 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   Map worldData;
-  fetchWorldWideData()async{
+  fetchWorldWideData() async {
     http.Response response = await http.get('https://corona.lmao.ninja/all');
     setState(() {
       worldData = json.decode(response.body);
     });
   }
 
+  List countryData;
+  fetchCountryData() async {
+    http.Response response =
+        await http.get('https://corona.lmao.ninja/countries');
+    setState(() {
+      countryData = json.decode(response.body);
+      countryData.sort(
+          (a, b) => b['active'].compareTo(a['active']));
+    });
+  }
+
   @override
   void initState() {
     fetchWorldWideData();
+    fetchCountryData();
     super.initState();
   }
 
@@ -33,17 +45,52 @@ class _HomePageState extends State<HomePage> {
         title: Text("Covid-19 Tracker"),
       ),
       body: SingleChildScrollView(
-        child: Column(children: <Widget>[
+          child: Column(
+        children: <Widget>[
           // Container(
           //   height: 100,
           //   color: Colors.orange[100],
           //   alignment: Alignment.center,
           //   padding: EdgeInsets.all(10),
           //   child: Text('Text'),
-          // )
-          worldData == null?CircularProgressIndicator():WorldPanel(worldData: worldData),
-        ],)
-      ),
+          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'World status',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Regional',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          worldData == null
+              ? CircularProgressIndicator()
+              : WorldPanel(worldData: worldData),
+          Text(
+            'Most affected countries',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          countryData == null
+              ? CircularProgressIndicator()
+              : MostAffectedPanel(countryData: countryData),
+        ],
+      )),
     );
   }
 }
